3f668b84bbd1301a0349fc83076ef064398075a8
PC <PC@192.168.1.97>
2018-10-29 23:25:42 +0700

    Edit tc_environment3

--- src/selenium_api/topic_001_Check_Enviroment3.java
+++ src/selenium_api/topic_001_Check_Enviroment3.java
@@ -2,48 +2,28 @@ package selenium_api;
 
 import java.util.concurrent.TimeUnit;
 
-import org.junit.Assert;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import Actions.Demo_Alllink;
 
 public class topic_001_Check_Enviroment3 {
 	WebDriver driver;
 
 	@BeforeClass
-	public void beforeClass() {
-	    driver = new FirefoxDriver();
-		driver.get("https://cybermentor.ca/");
+	public void beforeClass() {					
+		System.setProperty("webdriver.chrome.driver",".\\lib\\chromedriver.exe");					
+		driver.get("http://demo.guru99.com/test/newtours/");
 		driver.manage().window().maximize();
-		//driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-	}
-
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		}
 	@Test
 	public void f() {
-		String title = driver.getTitle();
-		System.out.println(title);
-		
-	}
-	@Test
-	public void f1() {
-		String title = driver.getTitle();
-		System.out.println(title);
-		
-	}
-	@Test
-	public void f2() {
-		String title = driver.getTitle();
-		System.out.println(title);
-		
-	}
-	@Test
-	public void f11() {
-		String title = driver.getTitle();
-		System.out.println(title);
-		
+		Demo_Alllink.getlink(driver);
 	}
 
 	@AfterClass
